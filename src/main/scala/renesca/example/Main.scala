69457c355bab84d3e39ee3ab5ef58862e2eb033c
Felix Dietze <felix@beerleader.net>
2015-05-15 15:06:17 +0200

    put comments before statements, print result table

--- src/main/scala/renesca/example/Main.scala
+++ src/main/scala/renesca/example/Main.scala
@@ -9,9 +9,11 @@ import spray.http.BasicHttpCredentials
 object Main extends App {
   // set up database connection
   val credentials = BasicHttpCredentials("neo4j", "neo4j")
+  // RestService contains an ActorSystem to handle HTTP communication via spray-client
   val restService = new RestService("http://localhost:7474", Some(credentials))
 
-  val db = new DbService // interface for submitting single requests
+  // query interface for submitting single requests
+  val db = new DbService
   // dependency injection
   db.restService = restService
 
@@ -24,6 +26,7 @@ object Main extends App {
   }
 
 
+  // create example graph:  snake -eats-> dog
   db.query("CREATE (:ANIMAL {name:'snake'})-[:EATS]->(:ANIMAL {name:'dog'})")
 
   val tx = new Transaction
@@ -31,39 +34,60 @@ object Main extends App {
   tx.restService = restService
 
 
-  implicit val graph = tx.queryGraph("MATCH (n:ANIMAL)-[r]->() RETURN n,r")
   // query a subgraph from the database
-  val snake = graph.nodes.find(_.properties("name").asInstanceOf[StringPropertyValue] == "snake").get // access the graph like scala collections
+  implicit val graph = tx.queryGraph("MATCH (n:ANIMAL)-[r]->() RETURN n,r")
 
-  // useful methods to access the graph (require implicit val graph in scope)
+  // access the graph like scala collections
+  val snake = graph.nodes.find(_.properties("name").asInstanceOf[StringPropertyValue] == "snake").get
+
+  // useful methods to access the graph (requires implicit val graph in scope)
   // e.g.: neighbours,  successors,  predecessors,  inDegree,  outDegree,  degree, ...
-  val name = snake.neighbours.head.properties("name") match {
-    case StringPropertyValue(string) => string
-    case _                           => ???
-  }
+  val name = snake.neighbours.head.properties("name").asInstanceOf[StringPropertyValue].value
   println("Name of one snake neighbour: " + name) // prints "dog"
 
-  snake.labels += "REPTILE" // changes to the graph are tracked
+  // changes to the graph are tracked
+  snake.labels += "REPTILE"
   snake.properties("hungry") = true
 
-  val hippo = Node.local // creating a local Node (a Node the database does not know about yet)
-  hippo.labels += "ANIMAL" // changes to local Nodes are also tracked
+  // creating a local Node (a Node the database does not know about yet)
+  val hippo = Node.local
+
+  // changes to local Nodes are also tracked
+  hippo.labels += "ANIMAL"
   hippo.properties("name") = "hippo"
-  graph.nodes += hippo // add the local node to the Node Set
-  // create a new relation between a local and existing Node
+
+  // add the local node to the Node Set
+  graph.nodes += hippo
+
+  // create a new local relation between a local and existing Node
   graph.relations += Relation.local(snake, "EATS", hippo)
 
   // persist all tracked changes to the database and commit the transaction
-  tx.persistChanges(graph)
-  tx.commit()
-  //  ODO: tx.commit.persistChanges(graph) // not working yet
+  tx.commit.persistChanges(graph)
 
 
-  val animals = db.queryTable("MATCH (n:ANIMAL)-[r:EATS]->() RETURN n.name as name, COUNT(r) as eatcount")
-  //TODO: print instead of assert
-  assert(animals.columns == List("name", "eatcount"))
-  assert(animals.rows.maxBy(_.apply("eatcount").asInstanceOf[LongPropertyValue].value).apply("name").asInstanceOf[StringPropertyValue] == "snake")
+  // interpret query result as a table
+  val animals = db.queryTable("MATCH (n:ANIMAL) OPTIONAL MATCH (n)-[r:EATS]->() RETURN n.name as name, COUNT(r) as eatcount")
 
+  println("\n" + animals.columns.mkString("\t")) // prints "name eatcount"
+  for(row <- animals.rows) {
+    print(row.cells(0).asInstanceOf[StringPropertyValue].value)
+    print("\t")
+    println(row.cells(1).asInstanceOf[LongPropertyValue].value)
+  }
+  println()
+  // loop prints:
+  //  dog	  0
+  //  snake	2
+  //  hippo	0
+
+  val hungriest = animals.rows.maxBy(_.apply("eatcount").asInstanceOf[LongPropertyValue].value).
+    apply("name").asInstanceOf[StringPropertyValue].value
+  println("hungriest: " + hungriest) // prints "snake"
+
+
+  // clear database
+  db.query("MATCH (n) OPTIONAL MATCH (n)-[r]-() DELETE n,r")
 
   // shut down actor system
   restService.system.shutdown()
